2842240a83d2de7a3eda99469bd3c436224fbdf4
Philipp Fleischer <privat@butchey.me>
2017-02-04 16:43:03 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  // Insertpoint is 'src/index.js', that's where the webpack magic begins
+  entry: path.join(__dirname, 'src', 'index.js'),
+  // The compiled file goes to 'dist/bundle.js'
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: path.join(__dirname, 'src'),
+        // Transform it with babel
+        loader: 'babel-loader',
+        // don't transform node_modules folder (which don't need to be compiled)
+        presets: ['es2017']
+      }
+    ]
+  }
+}
